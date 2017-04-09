3e664dc3ab180fb5b1dad576980db1ce5f7a1600
Zachary Durland <zacharydurland@gmail.com>
2017-04-09 15:06:25 +0900

    add card styling and field clearing
    Display results as cards and clear the search field when clicked on/

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 document.addEventListener("touchstart", function(){}, true);
-$(document).ready(function(){
-    
+$(document).ready(function(){ 
     var currPage = $("#results").text();
-    
     var hasResults = function(){
         if( currPage === "") {
             console.log("Page is empty")
@@ -12,9 +10,13 @@ $(document).ready(function(){
             return true;
         }
     };
+    $("#user-query-field").click(function(){
+        $(this).attr("value","");  
+        $(this).val("");  
+    });
     
     var searchAndDisplay = function(){
-        var userQuery = $("#userQueryForm").val();
+        var userQuery = $("#user-query-field").val();
         var jsonUrl= "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userQuery;
             $.getJSON(jsonUrl, function(data) {
 
@@ -23,19 +25,13 @@ $(document).ready(function(){
 
                 //print the result title    
                 console.log(data[1][i]);
-                $("#results").append("<div class='result'><h3>" + data[1][i] + "</h3>")
-                //print the summary
-                console.log(data[2][i]);
-                $("#results").append("<p>" + data[2][i] + "</p>")
-                //print the url   
-                console.log(data[3][i]);
-                $("#results").append("<p><a href=" + data[3][i] + ">View on Wikipedia</a><p></div>")
-
+                $("#results").append("<article class='result'><h3>" + data[1][i] + "</h3><p>" + data[2][i] + "</p><p><a href='" + data[3][i] + "'></a></p></article>" )
+        
                 }
             }, "json"); 
     }
 
-    $("button#searchButton").click(function(){
+    $("button#search-button").click(function(){
         
         if(hasResults){
             $("#results").html("");
