86e9e21bd47aad00ec1408b75097551764c0d22d
Zachary Durland <zacharydurland@gmail.com>
2017-04-10 12:55:25 +0900

    add random article title and link to hello card

--- js/main.js
+++ js/main.js
@@ -30,42 +30,44 @@ $(document).ready(function(){
         var userQuery = $("#user-query-field").val();
         var jsonUrl= "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userQuery;
             $.getJSON(jsonUrl, function(data) {
-
-                console.log(data);
                 for(i=0; i < data[1].length; i++) {
-
                 //print the result title    
                 console.log(data[1][i]);
                 $("#results").append('<article class="card result"><h2>' + data[1][i] + '</h2><p>' + data[2][i] + '</p><p><a href="' + data[3][i] + '" target="_blank">View on Wikipedia</a> <i class="fa fa-external-link" aria-hidden="true"></i></p></article>')
-                
                 showResults();
-        
                 }
             }, "json"); 
-        
-        
-    }
+    };
+    
+    var randomArticle = function(){
+        var jsonUrl= "https://cors-anywhere.herokuapp.com/http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&titles=Main%20Page&prop=info&inprop=url&format=json";
+            $.getJSON(jsonUrl, function(data) {
+                console.log(data);
+                //print the random article    
+                $("#hello-card").append('<span id="random-article"><a href="' + data.query.pages[Object.keys(data.query.pages)].fullurl + '" target="_blank"><h3>' + data.query.pages[Object.keys(data.query.pages)].title + ' <i class="fa fa-external-link" aria-hidden="true"></h3></a></span>');
+                
+            }, "json"); 
+    };
 
     $("button#search-button").on('click',function(){
-        
-       
         $(this).addClass("flip animated").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
             $(this).removeClass("flip");
         });
-     
-        
         if(hasResults){
             $("#results").html("");
             searchAndCreate();
-            
         } else {
             searchAndCreate();
         }
-        
     }); // end button search on click
     
-   
+    $(".one-time").click(function(){
+        randomArticle();
+    });
     
     
      
 }); //end main function
+
+
+//https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
